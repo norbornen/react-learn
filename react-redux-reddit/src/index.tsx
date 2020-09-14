e6bf7207a1cc69bc1974268591b6652e941b3b9c
Alexey Kolpakov <al.kolpak@gmail.com>
2020-09-15 01:52:36 +0300

    ->github

--- react-redux-reddit/src/index.tsx
+++ react-redux-reddit/src/index.tsx
@@ -1,12 +1,12 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import Root from './containers/Root';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Root />
   </React.StrictMode>,
   document.getElementById('root')
 );
